eb27f0be4c18979bf8ccdc1d061194b97d6e9d10
jonas van hoof <s136679@ap.be>
2023-05-03 23:05:58 +0200

    add some UI changes

---
+++ lib/provider/authentication_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../model/account.dart';
+
+class AuthenticationProvider extends ChangeNotifier {
+  Account? _user;
+
+  Account? get user => _user;
+
+  bool get loggedIn => _user != null;
+
+  logout() {
+    _user = null;
+  }
+
+  login(Account newName) {
+    _user = newName;
+    notifyListeners();
+  }
+}
