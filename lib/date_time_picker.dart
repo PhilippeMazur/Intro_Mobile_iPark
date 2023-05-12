901c524777129f9d01ec1a71fc6e80f31e9cd641
jonas van hoof <s136679@ap.be>
2023-05-12 18:12:45 +0200

    place datetimepicker decoration out component

--- lib/date_time_picker.dart
+++ lib/date_time_picker.dart
@@ -3,7 +3,9 @@ import 'package:intl/intl.dart';
 
 class DateTimePicker extends StatefulWidget {
   final Function(DateTime) setState;
-  const DateTimePicker({super.key, required this.setState});
+  final InputDecoration decoration;
+  const DateTimePicker(
+      {super.key, required this.setState, required this.decoration});
 
   @override
   State<DateTimePicker> createState() => _DateTimePickerState();
@@ -14,64 +16,45 @@ class _DateTimePickerState extends State<DateTimePicker> {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: textController,
-      obscureText: false,
-      textAlign: TextAlign.left,
-      maxLines: 1,
-      readOnly: true,
-      onTap: () async {
-        await showDatePicker(
-          context: context,
-          firstDate: DateTime(1900),
-          initialDate: DateTime.now(),
-          lastDate: DateTime(2100),
-        ).then((DateTime? date) async {
-          if (date != null) {
-            final time = await showTimePicker(
-              context: context,
-              initialTime: TimeOfDay.fromDateTime(DateTime.now()),
-            );
-            if (time != null) {
-              DateTime sum = DateTime.fromMillisecondsSinceEpoch(
-                  date.millisecondsSinceEpoch +
-                      (time.hour * 3600000 + time.minute * 60000));
-              textController.text = DateFormat('dd-MM-yyy hh:mm').format(date);
+        controller: textController,
+        obscureText: false,
+        textAlign: TextAlign.left,
+        maxLines: 1,
+        readOnly: true,
+        onTap: () async {
+          await showDatePicker(
+            context: context,
+            firstDate: DateTime(1900),
+            initialDate: DateTime.now(),
+            lastDate: DateTime(2100),
+          ).then((DateTime? date) async {
+            if (date != null) {
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.fromDateTime(DateTime.now()),
+              );
+              if (time != null) {
+                DateTime sum = DateTime.fromMillisecondsSinceEpoch(
+                    date.millisecondsSinceEpoch +
+                        (time.hour * 3600000 + time.minute * 60000));
+                textController.text =
+                    DateFormat('dd-MM-yyy hh:mm').format(date);
 
-              widget.setState(sum);
+                widget.setState(sum);
+              } else {
+                widget.setState(date);
+              }
             } else {
-              widget.setState(date);
+              widget.setState(DateTime.now());
             }
-          } else {
-            widget.setState(DateTime.now());
-          }
-        });
-      },
-      style: const TextStyle(
-        fontWeight: FontWeight.w400,
-        fontStyle: FontStyle.normal,
-        fontSize: 12,
-        color: Color(0xff000000),
-      ),
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(
-          // width: 0.0 produces a thin "hairline" border
-          borderRadius: BorderRadius.all(Radius.circular(90.0)),
-          borderSide: BorderSide.none,
-        ),
-        hintStyle: TextStyle(
+          });
+        },
+        style: const TextStyle(
           fontWeight: FontWeight.w400,
           fontStyle: FontStyle.normal,
-          fontSize: 14,
-          color: Color.fromARGB(255, 129, 129, 129),
+          fontSize: 12,
+          color: Color(0xff000000),
         ),
-        hintText: "datum",
-        filled: true,
-        fillColor: Color(0xfff2f2f3),
-        isDense: false,
-        contentPadding: EdgeInsets.fromLTRB(12, 8, 12, 8),
-        suffixIcon:
-            Icon(Icons.calendar_today, color: Color(0xff212435), size: 25),
-      ),
-    );
+        decoration: widget.decoration);
   }
 }
