be71fbba46a6d86a7ffc7ba9f9569b47d58cedd4
jonas van hoof <s136679@ap.be>
2023-05-17 15:06:49 +0200

    add 'total' stats

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,11 +1,68 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'custom_app_bar.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      appBar: CustomAppBar(
+        title: "Profiel",
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Text("profile picture"),
+            IntrinsicHeight(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Text(
+                        "35",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900, fontSize: 40),
+                      ),
+                      Text("keer verhuurd")
+                    ],
+                  )),
+                  const VerticalDivider(
+                    width: 20,
+                    thickness: 2,
+                    indent: 20,
+                    endIndent: 0,
+                    color: Colors.grey,
+                  ),
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Text(
+                        "0",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900, fontSize: 40),
+                      ),
+                      Text("keer gehuurd")
+                    ],
+                  )),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "geschidenis",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
